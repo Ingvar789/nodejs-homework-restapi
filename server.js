2207774aa3375df38c1f46190470d3f42170f567
Ihor Bodianchuk <ihordobruy@gmail.com>
2023-06-05 20:12:25 +0300

    fix process exit

--- server.js
+++ server.js
@@ -5,5 +5,8 @@ mongoose.connect(DB_HOST)
     .then(() => app.listen(3000, () => {
       console.log("Database connection successful")
     }))
-    .catch((error) => console.log(error.message));
+    .catch((error) => {
+        console.log(error.message);
+        process.exit(1);
+    });
 

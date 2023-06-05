f111433cd8bc9f1fff68dcd05d31a932fe4bca26
Ihor Bodianchuk <ihordobruy@gmail.com>
2023-06-05 20:02:46 +0300

    put DB_HOST in another file

---
+++ config.js
@@ -0,0 +1,4 @@
+const DB_HOST = "mongodb+srv://Ihor:U6LfIGzxo0uUsyeJ@cluster0.c3popry.mongodb.net/contacts?retryWrites=true&w=majority";
+module.exports = {
+    DB_HOST,
+}
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const app = require('./app')
+const mongoose = require("mongoose");
+const {DB_HOST} = require("./config");
+console.log(process.env);
+mongoose.connect(DB_HOST)
+    .then(() => app.listen(3000, () => {
+      console.log("Database connection successful")
+    }))
+    .catch((error) => console.log(error.message));
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
